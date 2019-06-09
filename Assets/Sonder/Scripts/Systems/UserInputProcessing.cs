48f8b15816a414da479b637b863acf9bd3edce31
gasolineJoe <39306361+gasolineJoe@users.noreply.github.com>
2019-06-09 10:46:08 +0600

    [del] removed all old ai and input code

--- Assets/Sonder/Scripts/Systems/UserInputProcessing.cs
+++ Assets/Sonder/Scripts/Systems/UserInputProcessing.cs
@@ -8,32 +8,7 @@ public class UserInputProcessing : IEcsRunSystem {
     EcsFilter<ObjectUser, InputControlled> _controlledUsers = null;
     EcsFilter<Storage, InputControlled> _controlledStorages = null;
 
-    float _xAxis;
-
     public void Run() {
-        _xAxis = Input.GetAxis("Horizontal");
-        for (var i = 0; i < _controlledEntities.EntitiesCount; i++) {
-            _controlledEntities.Components1[i].Acceleration = _xAxis;
-        }
-
-        if (Input.GetKeyDown(KeyCode.F)) {
-            for (var i = 0; i < _controlledUsers.EntitiesCount; i++) {
-                _controlledUsers.Components1[i].UsePressed = true;
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.I)) {
-            for (var i = 0; i < _controlledStorages.EntitiesCount; i++) {
-                String result = "inventory: ";
-                for (var j = 0; j < _controlledStorages.Components1[i].Inventory.Count; j++) {
-                    result += _controlledStorages.Components1[i].Inventory[j] + " ";
-                }
-
-                if (_controlledStorages.Components1[i].Inventory.Count == 0) {
-                    result += "empty";
-                }
-                Debug.Log(result);
-            }
-        }
+        
     }
 }

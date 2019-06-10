cbb6dcfa58faf664e79686092c54b18510ba190d
gasolineJoe <39306361+gasolineJoe@users.noreply.github.com>
2019-06-10 19:37:49 +0600

    [upd] mouse movement system

--- Assets/Sonder/Scripts/Systems/UserInputProcessing.cs
+++ Assets/Sonder/Scripts/Systems/UserInputProcessing.cs
@@ -1,15 +1,26 @@
 ﻿using LeopotamGroup.Ecs;
 using Sonder.Scripts.Components.Abilities;
+using Sonder.Scripts.Components.Abilities.Mind;
+using Sonder.Scripts.Components.Entities;
+using UnityEngine;
 
 namespace Sonder.Scripts.Systems {
     [EcsInject]
     public class UserInputProcessing : IEcsRunSystem {
-        EcsFilter<Movable, InputControlled> _controlledEntities = null;
+        EcsFilter<Human, Movable, InputControlled> _controlledEntities = null;
         EcsFilter<ObjectUser, InputControlled> _controlledUsers = null;
         EcsFilter<Storage, InputControlled> _controlledStorages = null;
+        private readonly Camera _camera = Object.FindObjectOfType<Camera>();
 
         public void Run() {
-        
+            if (Input.GetMouseButtonDown(0)) {
+                var point = _camera.ScreenToWorldPoint(Input.mousePosition);
+                for (var i = 0; i < _controlledEntities.EntitiesCount; i++) {
+                    var human = _controlledEntities.Components1[i];
+                    human.ActionQueue.Interrupt();
+                    human.ActionQueue.AddAction(Action.Walk, point.x);
+                }
+            }
         }
     }
 }
